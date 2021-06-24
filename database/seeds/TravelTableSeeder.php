770c3bb319743b59e4ea141a1638b4c9e51d14a2
mattiatealdi <mattiatealdi@gmail.com>
2021-06-24 16:15:03 +0200

    commit

--- database/seeds/TravelTableSeeder.php
+++ database/seeds/TravelTableSeeder.php
@@ -19,6 +19,8 @@ class TravelTableSeeder extends Seeder
             $new_travel->date = '2018-06-12';
             $new_travel->duration = 21;
             $new_travel->hotels = 'Hilton, Da Gino';
+            $new_travel->price = 10000;
+            $new_travel->number_of_children = 3;
             $new_travel->description = 'Vacanza di Mare in Francia con gita a Parigi';
             $new_travel->save();
 
